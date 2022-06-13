3d073bfdfd5ab99bc6ab9d40fcac9f640fbcf94d
Shaw Lu <shawlu95@gmail.com>
2022-06-13 15:49:53 -0700

    Ledger service: implement transaction delete and unit tests

---
+++ ledger/src/routes/__test__/transaction-delete.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { StatusCodes } from 'http-status-codes';
+import { natsWrapper } from '../../nats-wrapper';
+import { EntryType } from '@bookkeeping/common';
+import { Account, AccountType } from '../../model/account';
+import { Transaction } from '../../model/transaction';
+import { buildTransaction } from './transaction-test-util';
+
+it('returns 400 if not signed in', async () => {
+  await request(app)
+    .delete('/api/transaction')
+    .send()
+    .expect(StatusCodes.UNAUTHORIZED);
+});
+
+it('returns 400 if id is missing', async () => {
+  await request(app)
+    .delete('/api/transaction')
+    .set('Cookie', global.signin())
+    .send()
+    .expect(StatusCodes.BAD_REQUEST);
+});
+
+it('returns 404 if transaction is not found', async () => {
+  const randomId = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .delete('/api/transaction')
+    .set('Cookie', global.signin())
+    .send({ id: randomId })
+    .expect(StatusCodes.NOT_FOUND);
+});
+
+it('returns 401 if not owner of transaction', async () => {
+  const { transaction } = await buildTransaction();
+  await request(app)
+    .delete('/api/transaction')
+    .set('Cookie', global.signin())
+    .send({ id: transaction.id })
+    .expect(StatusCodes.UNAUTHORIZED);
+});
+
+it('returns 200 if delete successfully', async () => {
+  const { userId, transaction } = await buildTransaction();
+  await request(app)
+    .delete('/api/transaction')
+    .set('Cookie', global.signin(userId))
+    .send({ id: transaction.id })
+    .expect(StatusCodes.OK);
+
+  const deleted = await Transaction.findById(transaction.id);
+  expect(deleted).toBeNull();
+});
--- ledger/src/routes/transaction-delete.ts
+++ ledger/src/routes/transaction-delete.ts
@@ -1,13 +1,43 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Account } from '../model/account';
 import { StatusCodes } from 'http-status-codes';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@bookkeeping/common';
+import { Transaction } from '../model/transaction';
 
 const router = express.Router();
 
-router.delete('/api/transaction', async (req: Request, res: Response) => {
-  console.log('delete transaction');
-  return res.status(StatusCodes.OK).send();
-});
+const validators = [
+  body('id').not().isEmpty().withMessage('Please provide transaction id'),
+];
+
+router.delete(
+  '/api/transaction',
+  requireAuth,
+  validators,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { id } = req.body;
+    const transaction = await Transaction.findById(id);
+
+    if (!transaction) {
+      throw new NotFoundError();
+    }
+
+    if (transaction.userId != req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    await Transaction.deleteOne({ id });
+    return res.status(StatusCodes.OK).send();
+  }
+);
 
 export { router as transactionDelete };
