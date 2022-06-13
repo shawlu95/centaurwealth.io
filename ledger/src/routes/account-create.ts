72a76bcdd0125105bb8c0df69a71910072219ea3
Shaw Lu <shawlu95@gmail.com>
2022-06-13 03:09:26 -0700

    Ledger service: implement account create test and route

--- ledger/src/routes/account-create.ts
+++ ledger/src/routes/account-create.ts
@@ -1,13 +1,44 @@
-import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { Account } from '../model/account';
+import { body } from 'express-validator';
+import { Account, AccountType } from '../model/account';
 import { StatusCodes } from 'http-status-codes';
+import {
+  BadRequestError,
+  requireAuth,
+  validateRequest,
+} from '@bookkeeping/common';
 
 const router = express.Router();
 
-router.post('/api/account', async (req: Request, res: Response) => {
-  console.log('create account');
-  return res.status(StatusCodes.OK).send();
-});
+const validators = [
+  body('name').not().isEmpty().withMessage('Please provide account name'),
+  body('type').not().isEmpty().withMessage('Please provide account type'),
+  body('type').custom((type: string) =>
+    Object.values(AccountType).includes(type as AccountType)
+  ),
+];
+
+router.post(
+  '/api/account',
+  requireAuth,
+  validators,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { name, type } = req.body;
+    const userId = req.currentUser!.id;
+    const exist = await Account.findOne({ name, userId });
+    if (exist) {
+      throw new BadRequestError('Account already exists');
+    }
+
+    const account = Account.build({
+      name,
+      userId,
+      type: type as AccountType,
+    });
+    await account.save();
+    return res.status(StatusCodes.OK).send({ id: account.id });
+  }
+);
 
 export { router as accountCreate };
