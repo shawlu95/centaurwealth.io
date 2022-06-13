e10407cabf319be4f954c1153672f7d0800c4ed3
Shaw Lu <shawlu95@gmail.com>
2022-06-13 04:53:51 -0700

    Ledger service: implement transaction create route and unit tests

---
+++ ledger/src/routes/__test__/transaction-create.test.ts
@@ -0,0 +1,164 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { app } from '../../app';
+import { StatusCodes } from 'http-status-codes';
+import { natsWrapper } from '../../nats-wrapper';
+import { Account, AccountType } from '../../model/account';
+import { EntryType } from '@bookkeeping/common';
+import { Transaction } from '../../model/transaction';
+
+it('returns 400 when not signed in', async () => {
+  await request(app)
+    .post('/api/transaction')
+    .send()
+    .expect(StatusCodes.UNAUTHORIZED);
+});
+
+it('returns 401 when memo is missing', async () => {
+  await request(app)
+    .post('/api/transaction')
+    .set('Cookie', global.signin())
+    .send({ userId: 'foo', memo: '' })
+    .expect(StatusCodes.BAD_REQUEST);
+
+  await request(app)
+    .post('/api/transaction')
+    .set('Cookie', global.signin())
+    .send({ userId: 'foo' })
+    .expect(StatusCodes.BAD_REQUEST);
+});
+
+it('returns 400 when trying to use others account', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const cash = Account.build({
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    name: 'cash',
+    type: AccountType.Asset,
+  });
+  await cash.save();
+
+  const expense = Account.build({
+    userId,
+    name: 'expense',
+    type: AccountType.Asset,
+  });
+  await expense.save();
+
+  await request(app)
+    .post('/api/transaction')
+    .set('Cookie', global.signin(userId))
+    .send({
+      userId,
+      memo: 'beer',
+      entries: [
+        {
+          amount: 10,
+          type: EntryType.Credit,
+          accountId: cash.id,
+          accountName: cash.name,
+          accountType: cash.type,
+        },
+        {
+          amount: 10,
+          type: EntryType.Debit,
+          accountId: expense.id,
+          accountName: expense.name,
+          accountType: expense.type,
+        },
+      ],
+    })
+    .expect(StatusCodes.UNAUTHORIZED);
+});
+
+it('returns 401 when debit != credit', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const cash = Account.build({
+    userId,
+    name: 'cash',
+    type: AccountType.Asset,
+  });
+  await cash.save();
+
+  const expense = Account.build({
+    userId,
+    name: 'expense',
+    type: AccountType.Asset,
+  });
+  await expense.save();
+
+  await request(app)
+    .post('/api/transaction')
+    .set('Cookie', global.signin(userId))
+    .send({
+      userId,
+      memo: 'beer',
+      entries: [
+        {
+          amount: 10,
+          type: EntryType.Credit,
+          accountId: cash.id,
+          accountName: cash.name,
+          accountType: cash.type,
+        },
+        {
+          amount: 5,
+          type: EntryType.Debit,
+          accountId: expense.id,
+          accountName: expense.name,
+          accountType: expense.type,
+        },
+      ],
+    })
+    .expect(StatusCodes.BAD_REQUEST);
+});
+
+it('returns 200 with successful transaction', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  const cash = Account.build({
+    userId,
+    name: 'cash',
+    type: AccountType.Asset,
+  });
+  await cash.save();
+
+  const expense = Account.build({
+    userId,
+    name: 'expense',
+    type: AccountType.Asset,
+  });
+  await expense.save();
+
+  const {
+    body: { id },
+  } = await request(app)
+    .post('/api/transaction')
+    .set('Cookie', global.signin(userId))
+    .send({
+      userId,
+      memo: 'beer',
+      entries: [
+        {
+          amount: 10,
+          type: EntryType.Credit,
+          accountId: cash.id,
+          accountName: cash.name,
+          accountType: cash.type,
+        },
+        {
+          amount: 10,
+          type: EntryType.Debit,
+          accountId: expense.id,
+          accountName: expense.name,
+          accountType: expense.type,
+        },
+      ],
+    })
+    .expect(StatusCodes.OK);
+
+  const transaction = await Transaction.findById(id);
+  expect(transaction).toBeDefined();
+  expect(transaction!.userId).toEqual(userId);
+});
--- ledger/src/routes/transaction-create.ts
+++ ledger/src/routes/transaction-create.ts
@@ -1,13 +1,40 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Account } from '../model/account';
+import { Transaction } from '../model/transaction';
 import { StatusCodes } from 'http-status-codes';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@bookkeeping/common';
 
 const router = express.Router();
 
-router.post('/api/transaction', async (req: Request, res: Response) => {
-  console.log('create transaction');
-  return res.status(StatusCodes.OK).send();
-});
+const validators = [
+  body('memo').not().isEmpty().withMessage('Please provide transaction memo'),
+];
+
+router.post(
+  '/api/transaction',
+  requireAuth,
+  validators,
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { memo, entries } = req.body;
+    const userId = req.currentUser!.id;
+
+    const transaction = Transaction.build({
+      userId,
+      memo,
+      entries,
+    });
+    await transaction.save();
+    return res.status(StatusCodes.OK).send({ id: transaction.id });
+  }
+);
 
 export { router as transactionCreate };
