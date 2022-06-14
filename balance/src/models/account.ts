22381414ed3eaabd4dc8884f3eed67ef610bfab6
Shaw Lu <shawlu95@gmail.com>
2022-06-13 19:05:06 -0700

    Balance sheet service: create Account model (replicate)
    Must have id field explicitly set to replicate data from ledger service.

---
+++ balance/src/models/account.ts
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+import { AccountType } from '@bookkeeping/common';
+
+interface AccountAttrs {
+  id: string; // required to replicate object
+  userId: string;
+  name: string;
+  type: AccountType;
+}
+
+interface AccountModel extends mongoose.Model<AccountDoc> {
+  build(attrs: AccountAttrs): AccountDoc;
+}
+
+interface AccountDoc extends mongoose.Document {
+  userId: string;
+  name: string;
+  type: AccountType;
+  debit: number;
+  credit: number;
+}
+
+const accountSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: Object.values(AccountType),
+    },
+    debit: {
+      type: Number,
+      default: 0,
+    },
+    credit: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+accountSchema.statics.build = (attrs: AccountAttrs) => {
+  return new Account({
+    _id: attrs.id,
+    ...attrs,
+  });
+};
+
+const Account = mongoose.model<AccountAttrs, AccountModel>(
+  'Account',
+  accountSchema
+);
+
+export { Account, AccountType };
