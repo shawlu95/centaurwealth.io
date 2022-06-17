835267e5199f584ce48bda375ed9da67d109a104
Shaw Lu <shawlu95@gmail.com>
2022-06-16 22:53:23 -0700

    Update domain to microservice-ticketing-app.xyz

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on server
     return axios.create({
-      baseURL: 'http://centaurwealth.io',
+      baseURL: 'http://microservice-ticketing-app.xyz',
       headers: req.headers,
     });
   } else {
