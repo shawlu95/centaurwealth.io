e369198f812bf6201d5faf94aaacb7c5dfa4d7eb
Shaw Lu <shawlu95@gmail.com>
2022-06-18 16:37:31 -0700

    Ledger service: create default accounts after signup

---
+++ ledger/src/events/listeners/user-signup-listener.ts
@@ -0,0 +1,48 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  AccountType,
+  UserSignupEvent,
+} from '@bookkeeping/common';
+import { AccountCreatedPublisher } from '../publishers/account-created-publisher';
+import { queueGroupName } from './queue-group-name';
+import { Account } from '../../model/account';
+import { natsWrapper } from '../../nats-wrapper';
+
+const defaultAccounts = {
+  asset: ['Checking Account', 'Savings Account', 'Cash'],
+  liability: ['Credit Card Debt', 'Loan'],
+  equity: ['Retained Earning'],
+  temporary: ['Expense', 'Salary'],
+};
+
+export class UserSignupListener extends Listener<UserSignupEvent> {
+  readonly subject = Subjects.UserSignup;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: UserSignupEvent['data'], msg: Message) {
+    const userId = data.id;
+    const publisher = new AccountCreatedPublisher(natsWrapper.client);
+
+    for (const [type, names] of Object.entries(defaultAccounts)) {
+      for (var i in names) {
+        const account = Account.build({
+          userId,
+          name: names[i],
+          type: type as AccountType,
+        });
+        await account.save();
+
+        publisher.publish({
+          id: account.id,
+          userId: account.userId,
+          name: account.name,
+          type: account.type,
+        });
+      }
+    }
+
+    msg.ack();
+  }
+}
