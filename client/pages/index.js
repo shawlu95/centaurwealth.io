80ff2f04f3a3552414209cb8c799e637a8964c09
Shaw Lu <shawlu95@gmail.com>
2022-06-18 00:55:49 -0700

    Client: format balance table

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,22 +3,41 @@ import Router from 'next/router';
 
 // Not allowed to fetch data in component in server-side render
 const LandingPage = ({ currentUser, accounts }) => {
-  const accountList = accounts
-    .filter((account) => account.balance != 0)
-    .map((account) => {
-      return (
-        <tr key={account.id}>
-          <td>{account.name}</td>
-          <td>{account.type}</td>
-          <td>{account.balance}</td>
-          <td>
-            <Link href='/accounts/[accountId]' as={`/accounts/${account.id}`}>
-              <a>View</a>
-            </Link>
-          </td>
-        </tr>
-      );
-    });
+  const getSection = (type) => {
+    const body = accounts
+      .filter((account) => account.type == type)
+      .filter((account) => account.balance > 0)
+      .map((account) => {
+        return (
+          <tr key={account.id}>
+            <td width='40%'>{account.name}</td>
+            <td width='40%'>{account.balance}</td>
+            <td>
+              <Link href='/accounts/[accountId]' as={`/accounts/${account.id}`}>
+                <a>View</a>
+              </Link>
+            </td>
+          </tr>
+        );
+      });
+
+    return (
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>{type.toUpperCase()}</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>{body}</tbody>
+      </table>
+    );
+  };
+
+  const asset = getSection('asset');
+  const liability = getSection('liability');
+  const equity = getSection('equity');
+  const temporary = getSection('temporary');
   return (
     <div>
       <h3>Balance Sheet</h3>
@@ -28,16 +47,10 @@ const LandingPage = ({ currentUser, accounts }) => {
       >
         New Account
       </button>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Type</th>
-            <th>Balance</th>
-          </tr>
-        </thead>
-        <tbody>{accountList}</tbody>
-      </table>
+      {asset}
+      {liability}
+      {equity}
+      {temporary}
     </div>
   );
 };
