26d76d0491da0b2d43328126798fbbf92220dc38
Shaw Lu <shawlu95@gmail.com>
2022-06-20 00:14:44 -0700

    Client: account create layout

--- client/pages/accounts/create.js
+++ client/pages/accounts/create.js
@@ -24,28 +24,30 @@ const AccountsCreate = () => {
     <div>
       <h3>Create Account</h3>
       <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label>Account Tittle</label>
-          <input
-            className='form-control'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <div className='d-grid gap-2'>
+          <div className='form-group'>
+            <label>Account Tittle</label>
+            <input
+              className='form-control'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='form-group'>
+            <label>Account Type</label>
+            <select
+              className='form-control'
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value='asset'>Asset</option>
+              <option value='liability'>Liability</option>
+              <option value='equity'>Equity</option>
+              <option value='temporary'>Temporary</option>
+            </select>
+          </div>
+          {errors}
+          <button className='btn btn-primary'>Submit</button>
         </div>
-        <div className='form-group'>
-          <label>Account Type</label>
-          <select
-            className='form-control'
-            onChange={(e) => setType(e.target.value)}
-          >
-            <option value='asset'>Asset</option>
-            <option value='liability'>Liability</option>
-            <option value='equity'>Equity</option>
-            <option value='temporary'>Temporary</option>
-          </select>
-        </div>
-        {errors}
-        <button className='btn btn-primary'>Submit</button>
       </form>
     </div>
   );
