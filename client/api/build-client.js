0f4f982692ac51f3f8a4602bb002c0b7916b08d3
Shaw Lu <shawlu95@gmail.com>
2022-06-22 23:56:32 -0700

    Change domain to: centaurwealth.io

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const DEV_URL =
   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
-const PROD_URL = 'http://www.centaurmoney.com';
+const PROD_URL = 'http://www.centaurwealth.io';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
--- migration/config.js
+++ migration/config.js
@@ -2,6 +2,6 @@ const email = 'shawlu95@gmail.com';
 const password = '1234';
 const file = '/Users/shaw.lu/Downloads/2022-06-14_entries.csv';
 const host = 'http://centaurwealth.dev';
-// const host = 'http://www.centaurmoney.com';
+// const host = 'http://www.centaurwealth.io';
 
 module.exports = { email, password, file, host };
