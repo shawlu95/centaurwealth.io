488d4ad0371f1e606f4e82d99d3d400b6ba6ff0c
Shaw Lu <shawlu95@gmail.com>
2022-07-01 14:04:30 -0700

    Client: do not display zero budget

--- client/components/budgets.js
+++ client/components/budgets.js
@@ -15,39 +15,41 @@ const Budgets = ({ budgets }) => {
           </tr>
         </thead>
         <tbody>
-          {budgets.map((budget) => (
-            <React.Fragment key={budget.id}>
-              <tr>
-                <td width='35%'>
-                  <b>{budget.name}</b>
-                  <Link
-                    href='/budget/update/[budgetId]'
-                    as={`/budget/update/${budget.id}`}
-                  >
-                    <button className='btn btn-light btn-sm'>Edit</button>
-                  </Link>
-                  <Link href='/budget/[budgetId]' as={`/budget/${budget.id}`}>
-                    <button className='btn btn-light btn-sm'>View</button>
-                  </Link>
-                </td>
-                <td width='15%'>{usd.format(budget.monthly)}</td>
-                <td width='15%'>{usd.format(budget.quarterly)}</td>
-                <td width='15%'>{usd.format(budget.annual)}</td>
-              </tr>
-              <tr>
-                <td width='35%'></td>
-                <td width='15%'>
-                  {usd.format(budget.summary.monthly?.amount || 0)}
-                </td>
-                <td width='15%'>
-                  {usd.format(budget.summary.quarterly?.amount || 0)}
-                </td>
-                <td width='15%'>
-                  {usd.format(budget.summary.annual?.amount || 0)}
-                </td>
-              </tr>
-            </React.Fragment>
-          ))}
+          {budgets
+            .filter((budget) => budget.monthly > 0)
+            .map((budget) => (
+              <React.Fragment key={budget.id}>
+                <tr>
+                  <td width='35%'>
+                    <b>{budget.name}</b>
+                    <Link
+                      href='/budget/update/[budgetId]'
+                      as={`/budget/update/${budget.id}`}
+                    >
+                      <button className='btn btn-light btn-sm'>Edit</button>
+                    </Link>
+                    <Link href='/budget/[budgetId]' as={`/budget/${budget.id}`}>
+                      <button className='btn btn-light btn-sm'>View</button>
+                    </Link>
+                  </td>
+                  <td width='15%'>{usd.format(budget.monthly)}</td>
+                  <td width='15%'>{usd.format(budget.quarterly)}</td>
+                  <td width='15%'>{usd.format(budget.annual)}</td>
+                </tr>
+                <tr>
+                  <td width='35%'></td>
+                  <td width='15%'>
+                    {usd.format(budget.summary.monthly?.amount || 0)}
+                  </td>
+                  <td width='15%'>
+                    {usd.format(budget.summary.quarterly?.amount || 0)}
+                  </td>
+                  <td width='15%'>
+                    {usd.format(budget.summary.annual?.amount || 0)}
+                  </td>
+                </tr>
+              </React.Fragment>
+            ))}
         </tbody>
       </table>
     </div>
