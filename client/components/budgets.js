46ae08225d59d9d834885097b41977b129082ec6
Shaw Lu <shawlu95@gmail.com>
2022-07-01 13:00:04 -0700

    Client: add total budget & expense

--- client/components/budgets.js
+++ client/components/budgets.js
@@ -8,18 +8,17 @@ const Budgets = ({ budgets }) => {
       <table className='table'>
         <thead>
           <tr>
-            <th width='20%'>Category</th>
-            <th width='10%'></th>
-            <th width='20%'>Month</th>
-            <th width='20%'>Quarter</th>
-            <th width='20%'>Annual</th>
+            <th width='35%'>Category</th>
+            <th width='15%'>Month</th>
+            <th width='15%'>Quarter</th>
+            <th width='15%'>Annual</th>
           </tr>
         </thead>
         <tbody>
           {budgets.map((budget) => (
             <React.Fragment key={budget.id}>
               <tr>
-                <td width='20%'>
+                <td width='35%'>
                   <b>{budget.name}</b>
                   <Link
                     href='/budget/update/[budgetId]'
@@ -31,26 +30,76 @@ const Budgets = ({ budgets }) => {
                     <button className='btn btn-light btn-sm'>View</button>
                   </Link>
                 </td>
-                <td width='20%'>Budget</td>
-                <td width='20%'>{usd.format(budget.monthly)}</td>
-                <td width='20%'>{usd.format(budget.quarterly)}</td>
-                <td width='20%'>{usd.format(budget.annual)}</td>
+                <td width='15%'>{usd.format(budget.monthly)}</td>
+                <td width='15%'>{usd.format(budget.quarterly)}</td>
+                <td width='15%'>{usd.format(budget.annual)}</td>
               </tr>
               <tr>
-                <td width='20%'></td>
-                <td width='10%'>Spent</td>
-                <td width='20%'>
+                <td width='35%'></td>
+                <td width='15%'>
                   {usd.format(budget.summary.monthly?.amount || 0)}
                 </td>
-                <td width='20%'>
+                <td width='15%'>
                   {usd.format(budget.summary.quarterly?.amount || 0)}
                 </td>
-                <td width='20%'>
+                <td width='15%'>
                   {usd.format(budget.summary.annual?.amount || 0)}
                 </td>
               </tr>
             </React.Fragment>
           ))}
+        </tbody>
+      </table>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th width='35%'>Total</th>
+            <th width='15%'>Month</th>
+            <th width='15%'>Quarter</th>
+            <th width='15%'>Annual</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td width='35%'>
+              <b>Budget</b>
+            </td>
+            <td width='15%'>
+              {usd.format(budgets.reduce((s, x) => x.monthly + s, 0))}
+            </td>
+            <td width='15%'>
+              {usd.format(budgets.reduce((s, x) => x.quarterly + s, 0))}
+            </td>
+            <td width='15%'>
+              {usd.format(budgets.reduce((s, x) => x.annual + s, 0))}
+            </td>
+          </tr>
+          <tr>
+            <td width='35%'>
+              <b>Expense</b>
+            </td>
+            <td width='15%'>
+              {usd.format(
+                budgets
+                  .filter((x) => x.summary.monthly?.amount > 0)
+                  .reduce((s, x) => x.summary.monthly.amount + s, 0)
+              )}
+            </td>
+            <td width='15%'>
+              {usd.format(
+                budgets
+                  .filter((x) => x.summary.quarterly?.amount > 0)
+                  .reduce((s, x) => x.summary.quarterly.amount + s, 0)
+              )}
+            </td>
+            <td width='15%'>
+              {usd.format(
+                budgets
+                  .filter((x) => x.summary.annual?.amount > 0)
+                  .reduce((s, x) => x.summary.annual.amount + s, 0)
+              )}
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
