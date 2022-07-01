5f14ef2e0bd8abc52b2e5f2219f07011237d3671
Shaw Lu <shawlu95@gmail.com>
2022-07-01 12:39:33 -0700

    Budget: set hour, minute, second of aggregation boundary to zero

--- budget/src/routes/date-util.ts
+++ budget/src/routes/date-util.ts
@@ -1,38 +1,36 @@
-const monthStart = (date = new Date()): Date => {
-  return new Date(date.setDate(1));
+const monthStart = (now = new Date()): Date => {
+  const date = new Date(now.setDate(1));
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
-const nextMonth = (date = new Date()): Date => {
+const nextMonth = (now = new Date()): Date => {
   // if november, next month will roll over into next year
-  return new Date(date.getFullYear(), date.getMonth() + 1, 1);
+  const date = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
-const quarterStart = (date = new Date()): Date => {
-  const quarter = Math.floor(date.getMonth() / 3 + 1);
-  return new Date(date.setFullYear(date.getFullYear(), (quarter - 1) * 3, 1));
+const quarterStart = (now = new Date()): Date => {
+  const quarter = Math.floor(now.getMonth() / 3 + 1);
+  const date = new Date(
+    now.setFullYear(now.getFullYear(), (quarter - 1) * 3, 1)
+  );
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
-const nextQuarter = (date = new Date()): Date => {
-  const quarter = Math.floor(date.getMonth() / 3 + 1);
-  return new Date(date.setFullYear(date.getFullYear(), quarter * 3, 1));
+const nextQuarter = (now = new Date()): Date => {
+  const quarter = Math.floor(now.getMonth() / 3 + 1);
+  const date = new Date(now.setFullYear(now.getFullYear(), quarter * 3, 1));
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
-const halfYearStart = (date = new Date()): Date => {
-  const month = date.getMonth() <= 5 ? 0 : 6;
-  return new Date(date.setFullYear(date.getFullYear(), month, 1));
+const yearStart = (now = new Date()): Date => {
+  const date = new Date(now.setFullYear(now.getFullYear(), 0, 1));
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
-const nextHalfYear = (date = new Date()): Date => {
-  const month = date.getMonth() <= 5 ? 6 : 12;
-  return new Date(date.setFullYear(date.getFullYear(), month, 1));
-};
-
-const yearStart = (date = new Date()): Date => {
-  return new Date(date.setFullYear(date.getFullYear(), 0, 1));
-};
-
-const nextYear = (date = new Date()): Date => {
-  return new Date(date.setFullYear(date.getFullYear() + 1, 0, 1));
+const nextYear = (now = new Date()): Date => {
+  const date = new Date(now.setFullYear(now.getFullYear() + 1, 0, 1));
+  return new Date(date.setHours(0, 0, 0, 0));
 };
 
 export default {
@@ -40,8 +38,6 @@ export default {
   nextMonth,
   quarterStart,
   nextQuarter,
-  halfYearStart,
-  nextHalfYear,
   yearStart,
   nextYear,
 };
