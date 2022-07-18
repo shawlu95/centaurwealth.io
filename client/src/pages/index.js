34c1c6e8a07c6143a02ecff298a83f0df5110202
Shaw Lu <shawlu95@gmail.com>
2022-07-17 23:46:55 -0700

    Clean up debt

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,18 +1,8 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Timeline from '../components/timeline';
 
 const HomePage = () => {
-  useEffect(() => {
-    const getUser = async () => {
-      const { data } = await axios.get('/api/auth/currentuser');
-      // dispatch({ type: USER_LOAD, payload: { currentUser: data.currentUser } });
-      console.log(data);
-    };
-    getUser();
-  }, []);
-
   return (
     <div>
       <Timeline />
