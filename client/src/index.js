f1358aead42c34b1a7dc069b4a05b5c7083d05e4
Shaw Lu <shawlu95@gmail.com>
2022-07-17 21:31:29 -0700

    Use bootstrap css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './api/axios';
 import App from './App';
 
