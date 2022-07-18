cd5820dc2aea47cefa936cd83ad3df6bbf12fd63
Shaw Lu <shawlu95@gmail.com>
2022-07-18 14:36:17 -0700

    Refactor account component

--- client/src/components/account.js
+++ client/src/components/account.js
@@ -1,59 +1,46 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { handleChange } from '../features/account/accountSlice';
 
 const Account = () => {
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { account } = useSelector((store) => store.account);
 
   const isNew = account.id === undefined;
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    dispatch(handleChange({ name, value }));
   };
 
-  const createButton = <button className='btn btn-primary'>Create</button>;
-
-  const updateButton = <button className='btn btn-primary'>Update</button>;
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className='d-grid gap-2'>
-          <div className='form-group'>
-            <label>Account Name</label>
-            <input
-              value={account.name}
-              onChange={(e) => setName(e.target.value)}
-              className='form-control'
-              disabled={account && !account.mutable}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Account Type</label>
-            <select
-              value={account.type}
-              className='form-control'
-              disabled={!isNew}
-              onChange={(e) => setType(e.target.value)}
-            >
-              <option value='asset'>Asset</option>
-              <option value='liability'>Liability</option>
-              <option value='equity'>Equity</option>
-              <option value='temporary'>Temporary</option>
-            </select>
-          </div>
-          {isNew ? createButton : updateButton}
-          <button
-            type='button'
-            className='btn btn-secondary w-100'
-            onClick={() => navigate(-1)}
-          >
-            Cancel
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className='form-group'>
+        <label>Account Name</label>
+        <input
+          name='name'
+          value={account.name}
+          onChange={onChange}
+          className='form-control'
+          disabled={account && !account.mutable}
+        />
+      </div>
+      <div className='form-group'>
+        <label>Account Type</label>
+        <select
+          name='type'
+          value={account.type}
+          className='form-control'
+          disabled={!isNew}
+          onChange={onChange}
+        >
+          <option value='asset'>Asset</option>
+          <option value='liability'>Liability</option>
+          <option value='equity'>Equity</option>
+          <option value='temporary'>Temporary</option>
+        </select>
+      </div>
+    </>
   );
 };
 
--- client/src/pages/account/detail.js
+++ client/src/pages/account/detail.js
@@ -1,58 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { handleChange } from '../../features/account/accountSlice';
+import Account from '../../components/account';
 
 const AccountDetail = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { account } = useSelector((store) => store.account);
 
-  const isNew = account.id === undefined;
-
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    dispatch(handleChange({ name, value }));
-  };
-
   const onSubmit = async (e) => {
     e.preventDefault();
   };
-
-  const createButton = <button className='btn btn-primary'>Create</button>;
-
-  const updateButton = <button className='btn btn-primary'>Update</button>;
 
   return (
     <div>
       <form onSubmit={onSubmit}>
         <div className='container d-grid gap-2'>
-          <div className='form-group'>
-            <label>Account Name</label>
-            <input
-              name='name'
-              value={account.name}
-              onChange={onChange}
-              className='form-control'
-              disabled={account && !account.mutable}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Account Type</label>
-            <select
-              name='type'
-              value={account.type}
-              className='form-control'
-              disabled={!isNew}
-              onChange={onChange}
-            >
-              <option value='asset'>Asset</option>
-              <option value='liability'>Liability</option>
-              <option value='equity'>Equity</option>
-              <option value='temporary'>Temporary</option>
-            </select>
-          </div>
-          {isNew ? createButton : updateButton}
+          <Account />
           <button
             type='button'
             className='btn btn-secondary w-100'
@@ -60,6 +24,7 @@ const AccountDetail = () => {
           >
             Cancel
           </button>
+          <button className='btn btn-primary'>Update</button>;
         </div>
       </form>
     </div>
