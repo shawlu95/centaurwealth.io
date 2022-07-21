e52b3745e2916b1c8e5ad73226e25206628c3ec2
Shaw Lu <shawlu95@gmail.com>
2022-07-21 09:38:06 -0700

    Balance: deprecate current-user middleware, use authenticate

--- balance/src/routes/balance-current.ts
+++ balance/src/routes/balance-current.ts
@@ -1,22 +1,16 @@
 import express, { Request, Response } from 'express';
 import { Account } from '../models/account';
 import { StatusCodes } from 'http-status-codes';
-import { AccountType, requireAuth } from '@bookkeeping/common';
-
 const router = express.Router();
 
-router.get(
-  '/api/balance/current',
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const userId = req.currentUser!.id;
-    const accounts = await Account.find({ userId }).sort({
-      type: 1,
-      balance: -1,
-    });
-    const summary = await Account.summary(userId);
-    return res.status(StatusCodes.OK).send({ accounts, summary });
-  }
-);
+router.get('/api/balance/current', async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const accounts = await Account.find({ userId }).sort({
+    type: 1,
+    balance: -1,
+  });
+  const summary = await Account.summary(userId);
+  return res.status(StatusCodes.OK).send({ accounts, summary });
+});
 
 export { router as balanceCurrent };
