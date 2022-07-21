a3017397c525c242ebbfcbfe8612c43ba59c6d69
Shaw Lu <shawlu95@gmail.com>
2022-07-21 09:44:18 -0700

    Timeline: deprecate current-user middleware, use authenticate

--- timeline/src/routes/timeline-index.ts
+++ timeline/src/routes/timeline-index.ts
@@ -1,21 +1,16 @@
 import express, { Request, Response } from 'express';
 import { Point } from '../model/point';
 import { StatusCodes } from 'http-status-codes';
-import { requireAuth } from '@bookkeeping/common';
 
 const router = express.Router();
 
-router.get(
-  '/api/timeline',
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const userId = req.currentUser!.id;
-    const yearFirstDay = new Date(new Date().getFullYear(), 0, 1);
-    const start = req.query.start || yearFirstDay;
-    const query = { userId, date: { $gte: start } };
-    const points = await Point.find(query).sort('date');
-    return res.status(StatusCodes.OK).send({ points });
-  }
-);
+router.get('/api/timeline', async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+  const yearFirstDay = new Date(new Date().getFullYear(), 0, 1);
+  const start = req.query.start || yearFirstDay;
+  const query = { userId, date: { $gte: start } };
+  const points = await Point.find(query).sort('date');
+  return res.status(StatusCodes.OK).send({ points });
+});
 
 export { router as timelineRouter };
