1644407e1b77117368b33e652f2cd4586635b892
Shaw Lu <shawlu95@gmail.com>
2022-07-21 10:17:59 -0700

    Client: cleanup global axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -8,17 +8,4 @@ const global = axios.create({
   baseURL: PROD_URL,
 });
 
-global.interceptors.request.use(
-  (config) => {
-    const user = getFromLocalStorage('user');
-    if (user) {
-      config.headers.common['Authorization'] = `Bearer ${user.token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export default global;
