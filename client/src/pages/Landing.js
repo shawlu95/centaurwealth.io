d46730a7f00ccb77ab70c56e80cf3de33fe101fd
Shaw Lu <shawlu95@gmail.com>
2022-07-23 08:43:57 -0700

    Change landing text

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,6 +2,7 @@ import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { LANDING_TEXT } from '../utils/text';
 
 const Landing = () => {
   const { user } = useSelector((store) => store.user);
@@ -12,17 +13,7 @@ const Landing = () => {
           <h1>
             Centaur <span>Wealth</span>
           </h1>
-          <p>
-            While companies today use enterprise resource planning (ERP) systems
-            to keep track of widgets, contractual obligations, and employees,
-            the accounting system-and the laws that support it-require us to
-            convert just about everything into monetary value, and enter it into
-            a ledger system based on the 1.0-year-old double-entry bookkeeping
-            method. This is the very same system used by the Florentine
-            merchants of the 13th century and described by Luca Pacioli, the
-            "father of accounting," in his book Summa de Arithmetica, Geometria,
-            Proportioni et Proportionalita, published 1494.
-          </p>
+          <p>{LANDING_TEXT}</p>
           {!user && (
             <Link
               to='/auth/signup'
---
+++ client/src/utils/text.js
@@ -0,0 +1,2 @@
+export const LANDING_TEXT =
+  'Reinventing the 800-year-old double-entry bookkeeping method, made easy for personal finance and wealth management. This is the very same system used by the Florentine merchants of the 13th century.';
