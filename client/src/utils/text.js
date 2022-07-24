bf39f8970e9972aaa94d078593aa20d0f27b466a
Shaw Lu <shawlu95@gmail.com>
2022-07-23 19:55:37 -0700

    Update text.js

--- client/src/utils/text.js
+++ client/src/utils/text.js
@@ -1,2 +1,2 @@
 export const LANDING_TEXT =
-  'Reinventing the 800-year-old double-entry bookkeeping method, made easy for personal finance and wealth management. This is the very same system used by the Florentine merchants of the 13th century.';
+  'Reinventing the 800-year-old double-entry bookkeeping method, the very same system used by the Florentine merchants of the 13th century, made easy for personal finance and wealth management.';
